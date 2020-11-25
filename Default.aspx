ae30a8a85a191818a3a21259367c9939a8f14ec8
SimpleBeing <43241706+SimpleBeing@users.noreply.github.com>
2020-11-25 11:17:56 +0200

    Upload

---
+++ Default.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1 style="text-align: center; font-size: 100px;">Student List Viewer</h1>
+    </div>
+
+    <div class="row" id="display1">
+        <div class="col-md-4" style="text-align: center;">
+            <h2>Student Number</h2>
+            <p>
+                <asp:TextBox ID="tbSNumber" runat="server"></asp:TextBox>
+            </p>
+        </div>
+        <div class="col-md-4" style="text-align: center;">
+            <h2>Student Name</h2>
+            <p>
+                <asp:TextBox ID="tbSName" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                &nbsp;</p>
+            <h2>Student Password</h2>
+                <asp:TextBox ID="tbSPassword" runat="server"></asp:TextBox>
+            <p>
+                &nbsp;</p>
+            <p>
+                &nbsp;</p>
+            <p>
+                <asp:DropDownList ID="ddlStudentNum" runat="server">
+                </asp:DropDownList>
+            </p>
+        </div>
+        <div class="col-md-4" style="text-align: center;">
+            <h2>Student Mark</h2>
+            <p>
+                <asp:TextBox ID="tbSMark" runat="server"></asp:TextBox>
+            </p>
+        </div>
+    </div>
+    <asp:Panel ID="Panel1" runat="server" HorizontalAlign="Center">
+        <asp:Button ID="btnAdd" runat="server" Text="Add" Width="70px" OnClick="btnAdd_Click" />
+        <asp:Button ID="btnUpdate" runat="server" Text="Update" Width="70px" OnClick="btnUpdate_Click" />
+        <asp:Button ID="btnDelete" runat="server" Text="Delete" Width="70px" OnClick="btnDelete_Click" />
+    </asp:Panel>
+    <asp:Panel ID="Panel2" runat="server" HorizontalAlign="Center">
+        <asp:GridView ID="GridView1" runat="server" HorizontalAlign="Center"></asp:GridView>
+    </asp:Panel>
+    
+    </asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : Page
+{
+    string[] sl = { "Student number", "Student Name", "Mark", "Password" };
+    DatabaseHandler dbh;
+    SqlConnection sConn;
+    private SqlCommand sCmd;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        dbh = new DatabaseHandler("StudentList",sl);
+        sConn = dbh.getConn();
+        LoadData();
+    }
+
+    void LoadData()
+    {
+        GridView1.DataSource = dbh.getTableDataAs(new string[,] { { "Student number", "Student Name", "Mark", "Password" }, { "Student Number", "Student Name", "Student Mark", "Student Password" } });
+        GridView1.DataBind();
+        ddlStudentNum.Items.Clear();
+        List<string> ids = dbh.getTableIDs("StudentList");
+        for(int x = 0; x < ids.Count; ++x)
+        {
+            ddlStudentNum.Items.Add(ids[x]);
+        }
+    }
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        string[] temp = { "'"+tbSNumber.Text+"'", "'" + tbSName.Text + "'",  tbSMark.Text , "'" + tbSPassword.Text + "'" };
+        sConn.Open();
+        SqlCommand dbCMB = sConn.CreateCommand();
+        dbCMB = new SqlCommand("INSERT INTO StudentList Values ('" + tbSNumber.Text + "','" +
+          tbSName.Text + "'," + tbSMark.Text + ",'" + tbSPassword.Text + "')", sConn);
+        dbCMB.ExecuteNonQuery();
+        sConn.Close();
+        LoadData();
+    }
+
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        sConn.Open();
+        SqlCommand dbCMB = sConn.CreateCommand();
+        dbCMB = new SqlCommand("DELETE FROM StudentList WHERE [Student number] ='" + ddlStudentNum.Text + "'", sConn);
+        dbCMB.ExecuteNonQuery();
+        sConn.Close();
+        LoadData();
+    }
+
+    protected void btnUpdate_Click(object sender, EventArgs e)
+    {
+        sConn.Open();
+        SqlCommand dbCMB = sConn.CreateCommand();
+        dbCMB = new SqlCommand("UPDATE StudentList SET [Student number]='" + ddlStudentNum.Text + "',[Student Name]='" +
+            tbSName.Text + "',Mark=" + tbSMark.Text + ", Password = '"+tbSPassword.Text+"' WHERE [Student number]='" + ddlStudentNum.Text + "'", sConn);
+        dbCMB.ExecuteNonQuery();
+        sConn.Close();
+        LoadData();
+    }
+}
